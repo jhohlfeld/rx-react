64de91ae1e979ea38e0353177b5e9f3f3b4c5bda
Jakob Hohlfeld <jhohlfeld@polyban.io>
2016-05-17 20:25:51 +0200

    Add router @cycloidal/driver-router.
    The router works with uniloc. Once initialized (using routes and alias definition), a history object will be created in driver-router. The observable sequence on the route will begin sending updates as soon as history has decoded the url.
    
    Updates on the sequence are being sent for every observer that joins, even late. This is done with a hot observable.
    As long as no route is present, the rendering of the view is skipped.
    
    This way, we eliminate initialState from view().
    
    npm install after update

--- app/index.js
+++ app/index.js
@@ -2,47 +2,87 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Cycle from '@cycle/core'
 import { createReactDriver } from './lib/driver-react';
+import { createRouterDriver } from '@cycloidal/driver-router';
 import App from './components/app'
 
-const initialState = { todos: [] }
-
-function intent({ DOM }) {
+function intent({ DOM, router }) {
     return {
         addTodo: DOM.events('addTodo').filter(todo => todo.title !== ''),
-        removeTodo: DOM.events('removeTodo')
+        updateTodo: DOM.events('updateTodo'),
+        removeTodo: DOM.events('removeTodo'),
+        routeChanged: router.onRouteChanged
     }
 }
 
 const operations = {
+
     addTodo: newTodo => state => ({
+        ...state,
         todos: state.todos.concat(newTodo)
     }),
+
+    updateTodo: todoToUpdate => state => ({
+        ...state,
+        todos: state.todos.map(todo => todo.id === todoToUpdate.id ? todoToUpdate : todo)
+    }),
+
     removeTodo: idToRemove => state => ({
+        ...state,
         todos: state.todos.filter(todo => todo.id !== idToRemove)
+    }),
+
+    routeChanged: newRoute => state => ({
+        ...state,
+        route: newRoute
     })
+
 }
 
-function model(intents) {
+function model(intents, initialState) {
+
     const allOperations$ = Rx.Observable.merge(
+
+        // add state operations due to state
         Object.keys(intents).map(intent =>
-            intents[intent].map(item => operations[intent](item))))
+            intents[intent].map(item => operations[intent](item)))
+
+    )
 
     return allOperations$
         .scan((state, operation) => operation(state), initialState)
+
 }
 
 function view(state$) {
     return state$
-        .startWith(initialState)
-        .map(state => React.createElement(App, { todos: state.todos }))
+        .startWith(null)
+        .skipWhile(state => !state || !state.hasOwnProperty('route'))
+        .map(state => React.createElement(App, state))
 }
 
-function main({ DOM }) {
-    return {
-        DOM: view(model(intent({ DOM })))
+function main({ DOM, router }) {
+
+    const initialState = {
+        todos: [],
+        router
     }
+
+    return {
+        DOM: view(model(intent({ DOM, router }), initialState))
+    }
+
+}
+
+const routes = {
+    listTodos: 'GET /todos',
+    editTodo: 'GET /todo/:id'
+}
+
+const aliases = {
+    'GET /': 'listTodos'
 }
 
 Cycle.run(main, {
-    DOM: createReactDriver('#app')
+    DOM: createReactDriver('#app'),
+    router: createRouterDriver(routes, aliases)
 })
